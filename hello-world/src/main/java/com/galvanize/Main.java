338b28922c4321ed401aed6680908c9c910160f1
iisom <130263868+iisom@users.noreply.github.com>
2024-09-12 16:38:10 -0400

    Add files via upload

---
+++ hello-world/src/main/java/com/galvanize/Main.java
@@ -0,0 +1,16 @@
+package com.galvanize;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isActive(String status) {
+            // Implement your solution
+            String x = "active";
+
+            if (x.equals("active")){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    }
